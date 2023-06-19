a1cdb6e0d33e83716d03920ce6585c6e9082c45d
UNTAN-ECT <97281880+untan-ect@users.noreply.github.com>
2023-06-19 11:14:33 +0700

    Membuat Sensor_Arus.ino
    Untuk pembacaan arus pada Baterai Mobil, masih perlu kalibrasi

---
+++ PCB_Sensor/Sensor Arus/Sensor_Arus.ino
@@ -0,0 +1,44 @@
+#define Arus 21
+
+#define nilaiKoreksi 164 //mA
+#define iterasi 10000
+#define teganganReferensi 3300.0 //5000mv is for 5V
+#define bitResolusi 12
+
+float I = 0;
+unsigned long oldtime, oldtime2 = 0;
+unsigned long interval = 1000;
+
+void setup() {
+  Serial.begin(115200);   //Serial komunikasi untuk dihubungkan ke laptop/PC dengan baud rate 115200
+  delay(1000);
+}
+
+void loop() {
+  if ((millis() - oldtime) >= interval) {
+    Sensor_Arus();
+    oldtime = millis();
+  }
+}
+
+void Sensor_Arus() {
+  int nilaiSensor;
+  float voltage, current;
+  if ((millis() - oldtime2) >= interval) {
+    for (int i = 0; i <= iterasi; i++)
+    {
+      nilaiSensor = analogRead(Arus);
+      voltage = (nilaiSensor) * (teganganReferensi / (pow(2, bitResolusi) - 1)) - ( 0.5 * teganganReferensi) + nilaiKoreksi ;
+      current  = voltage / 66.0  ;
+      I += current;
+    }
+    oldtime2 = millis();
+  }
+  I /= iterasi;
+  I -= 21.17;
+  if (I < 0) {
+    I = 0;
+  }
+  Serial.println("Arus : " + String(I));
+  Serial.println("nilaiSensor : " + String(nilaiSensor));
+}
