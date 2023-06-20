3a1a958b88fbd4304b9546d6a6bc7ff0bd5eff6c
Rahmadiah15 <137176969+Rahmadiah15@users.noreply.github.com>
2023-06-20 19:09:06 +0700

    mengubah Sensor_Arus.ino
    tinggal kalibrasi

--- PCB_Sensor/Sensor Arus/Sensor_Arus.ino
+++ PCB_Sensor/Sensor Arus/Sensor_Arus.ino
@@ -1,4 +1,4 @@
-#define Arus 21
+#define Arus 35
 
 #define nilaiKoreksi 164 //mA
 #define iterasi 10000
@@ -9,36 +9,36 @@ float I = 0;
 unsigned long oldtime, oldtime2 = 0;
 unsigned long interval = 1000;
 
-void setup() {
+void setup() {  
   Serial.begin(115200);   //Serial komunikasi untuk dihubungkan ke laptop/PC dengan baud rate 115200
   delay(1000);
 }
 
 void loop() {
-  if ((millis() - oldtime) >= interval) {
+  if((millis() - oldtime) >= interval){
     Sensor_Arus();
     oldtime = millis();
   }
 }
 
-void Sensor_Arus() {
+void Sensor_Arus(){
   int nilaiSensor;
   float voltage, current;
-  if ((millis() - oldtime2) >= interval) {
-    for (int i = 0; i <= iterasi; i++)
-    {
-      nilaiSensor = analogRead(Arus);
-      voltage = (nilaiSensor) * (teganganReferensi / (pow(2, bitResolusi) - 1)) - ( 0.5 * teganganReferensi) + nilaiKoreksi ;
-      current  = voltage / 66.0  ;
-      I += current;
+  if((millis() - oldtime2) >= interval){
+    for(int i=0; i<= iterasi; i++)
+      {   
+        nilaiSensor = analogRead(Arus);
+          voltage = (nilaiSensor) * (teganganReferensi / (pow(2,bitResolusi)-1)) - ( 0.5 * teganganReferensi) + nilaiKoreksi ; 
+          current  = voltage / 66.0  ;
+          I += current;
     }
     oldtime2 = millis();
   }
   I /= iterasi;
-  I -= 21.17;
-  if (I < 0) {
-    I = 0;
-  }
-  Serial.println("Arus : " + String(I));
-  Serial.println("nilaiSensor : " + String(nilaiSensor));
+   I -= 2.59;
+//    if(I < 0){
+//      I = 0;
+//    }
+    Serial.println("Arus : " + String(I));
+    Serial.println("nilaiSensor : " + String(nilaiSensor));
 }
