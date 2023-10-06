8be9f1b19f515279b3d7e4ef798a745e1cf2146f
UNTAN-ECT <138673819+UNTAN-ElektricCarTeam@users.noreply.github.com>
2023-10-06 17:48:37 +0700

    Create Sensor_Kecepatan.ino

---
+++ PCB_Sensor/Sensor Kecepatan/Sensor_Kecepatan.ino
@@ -0,0 +1,44 @@
+#define Infra 18
+
+unsigned long oldtime = 0;
+unsigned long interval = 1000;
+unsigned long timeold = 0; //Deklarasi nilai variabel oldtime = 0
+int TIME; //Deklarasi Variabel time
+int rev = 0; //Deklarasi nilai variabel rev = 0
+int v = 0; //Kecepatan KM/Jam
+float pi = 3.14;
+float r = 0.03;
+float rpm; //Deklarasi Variabel
+float Speed = 0; //Deklarasi nilai variabel Speed = 0
+float rotasi;
+
+void isr()          //interrupt service routine
+{
+  rev++;
+}
+
+void setup() {
+  pinMode(Infra, INPUT);
+  Serial.begin(115200);
+  attachInterrupt(Infra, isr, RISING);
+}
+
+void loop() {
+  if ((millis() - oldtime) >= interval) {
+    detachInterrupt(Infra);
+    TIME = millis() - timeold;
+    rpm = rev / TIME * 60000 ;
+    timeold = millis();
+    Speed = rpm * ((2.0 * pi * r) / 60.0);
+    v = Speed * 3.6;
+    oldtime5 = millis();
+    Serial.println("Kecepatan rev = " + String(rev) + " putaran per detik");
+    Serial.println("Kecepatan rpm = " + String(rpm) + " putaran per menit");
+    Serial.println("Kecepatan speed = " + String(Speed) + " m/s");
+    Serial.println("Kecepatan v = " + String(v) + " km/jam");
+    Serial.println("===========================");
+    Serial.println(" ");
+    attachInterrupt(Infra, isr, RISING);
+    rev = 0;
+  }
+}
